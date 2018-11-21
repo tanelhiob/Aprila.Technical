ad15ff925aa1763de140b3a8a6eb62eedaf92622
Tanel Hiob <tanelhiob@outlook.com>
2018-11-21 20:49:12 +0200

    Added migrations; Fixed db DI in singleton

--- Aprila.Technical.Web/Services/Calculator.cs
+++ Aprila.Technical.Web/Services/Calculator.cs
@@ -8,13 +8,6 @@ namespace Aprila.Technical.Web.Services
 {
     public class Calculator : ICalculator
     {
-        //private readonly IServiceProvider _services;
-
-        //public Calculator(IServiceProvider services) 
-        //{
-        //    _services = services;
-        //}
-
         private readonly string _connectionString;
 
         public Calculator(string connectionString)
@@ -30,7 +23,7 @@ namespace Aprila.Technical.Web.Services
 
             using (var db = new MyDbContext(options))
             {
-                return a + b + (db.Users.FirstOrDefault()?.Name.Length ?? 0);
+                return a + b + (db.Users.LastOrDefault()?.Name.Length ?? 0);
             }
         }
     }
--- Aprila.Technical.Web/Startup.cs
+++ Aprila.Technical.Web/Startup.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Aprila.Technical.Web.Data;
+﻿using Aprila.Technical.Web.Data;
 using Aprila.Technical.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using System.Linq;
+using System.Text;
 
 namespace Aprila.Technical.Web
 {
@@ -17,24 +15,38 @@ namespace Aprila.Technical.Web
     {
        public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = "Data Source=|DataDirectory|\\Aprila.Technical.sqlite;";
+            var connectionString = "Data Source=App_Data\\Aprila.Technical.sqlite;";
             services.AddDbContextPool<MyDbContext>(opt => opt.UseSqlite(connectionString));
 
             services.AddSingleton<ICalculator, Calculator>(serviceProvider => new Calculator(connectionString));
        }
 
-        public void Configure(IApplicationBuilder app, ICalculator calculator)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
 
             app.Run(async (context) =>
             {
-                var query = context.Request.QueryString;
-                var queryDictionary = QueryHelpers.ParseNullableQuery(query.ToUriComponent());
-                var a = 1; //int.Parse(queryDictionary["a"]);
-                var b = 2; //int.Parse(queryDictionary["b"]);
-                var c = calculator.Add(a, b);
-                await context.Response.WriteAsync(c.ToString());
+                var responseMessage = new StringBuilder();
+                responseMessage.AppendLine(":P");
+                
+                var query = context.Request.Query;
+
+                if (query.TryGetValue("name", out StringValues name))
+                {
+                    var db = context.RequestServices.GetService<MyDbContext>();
+                    db.Add(new Domain.User { Id = System.Guid.NewGuid(), Name = name });
+                    await db.SaveChangesAsync();
+                }
+
+                if (query.TryGetValue("a", out StringValues a) && query.TryGetValue("b", out StringValues b))
+                {
+                    var calculator = context.RequestServices.GetService<ICalculator>();
+                    var c = calculator.Add(int.Parse(a.First()), int.Parse(b.First()));
+                    responseMessage.AppendLine(c.ToString());
+                }
+
+                await context.Response.WriteAsync(responseMessage.ToString());
             });
         }
     }
